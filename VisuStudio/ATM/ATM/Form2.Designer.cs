2dbcfc490e254924d722196ca1c1609780e59856
Felix <0911838@hr.nl>
2016-03-01 14:15:11 +0100

    First version of the visual App

---
+++ VisuStudio/ATM/ATM/Form2.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ATM
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DebugText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(92, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(797, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome, please insert PIN to continue";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DebugText
+            // 
+            this.DebugText.Location = new System.Drawing.Point(13, 13);
+            this.DebugText.Name = "DebugText";
+            this.DebugText.Size = new System.Drawing.Size(293, 20);
+            this.DebugText.TabIndex = 2;
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.BackgroundImage = global::ATM.Properties.Resources.Start_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1009, 309);
+            this.Controls.Add(this.DebugText);
+            this.Controls.Add(this.label1);
+            this.Name = "Start";
+            this.Text = "Start";
+            this.Load += new System.EventHandler(this.Start_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DebugText;
+    }
+}
---
+++ VisuStudio/ATM/ATM/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ATM
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            Cursor.Hide();
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            InitializeComponent();
+        }
+
+        private void Start_Load(object sender, EventArgs e)
+        {
+            SerialPort startPort = Arduino.getPort();
+            String s = startPort.ReadTo("\r\n");
+            if (startPort.ReadTo("\r\n") == "NEW")
+            {
+                DebugText.Text = "CARD FOUND";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
