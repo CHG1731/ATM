688c2592f3b90f9b6d862fbf8c9065e492f7ce08
Felix <0911838@hr.nl>
2016-03-26 15:21:38 +0100

    Config screen is done

--- Final version/Final_Application/Final_Application/Init1.cs
+++ Final version/Final_Application/Final_Application/Init1.cs
@@ -15,6 +15,72 @@ namespace Final_Application
         public Init1()
         {
             InitializeComponent();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            fetchCom();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Arduino.makePort(comboBox1.SelectedItem.ToString()) == true)
+                {
+                    StatusText.ForeColor = Color.Green;
+                    StatusText.Text = "ARDUINO CONNECTED";
+                    StartButton.Visible = true;
+                }
+                else
+                {
+                    StatusText.ForeColor = Color.Red;
+                    StatusText.Text = "NO CONNECTION";
+                }
+            }
+            catch (Exception)
+            {
+                Error.show("No port selected", "Error");
+            }
+        }
+
+        private void COMPORTCHOOSE_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void fetchCom()
+        {
+            String[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (String port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            if (ports.Length < 1)
+            {
+                comboBox1.Items.Add("No ports available");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KILLBUTTON_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Go to the next frame
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            fetchCom();
         }
     }
 }
--- Final version/Final_Application/Final_Application/Program.cs
+++ Final version/Final_Application/Final_Application/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,11 +23,46 @@ namespace Final_Application
 
     static class Arduino
     {
-
+        static SerialPort Ard = new SerialPort();
+        static String portName = "COM1";
+        public static bool makePort(String s)
+        {
+            portName = s;
+            bool status = false;
+            Ard.BaudRate = 9600;
+            Ard.PortName = portName;
+            try
+            {
+                Ard.Open();
+                status = true;
+            }
+            catch (System.IO.IOException)
+            {
+                Error.show("Poort niet gevonden", "Error");
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                Error.show("Toegang tot de compoort is geweigerd", "Error");
+            }
+            catch (Exception)
+            {
+                Error.show("No port selected", "Error");
+            }
+            return status;
+        }
+        public static SerialPort getPort()
+        {
+            return Ard;
+        }
     }
-    
+
     static class Error
     {
+        public static void show(String s, String x)
+        {
+            MessageBox.Show(s, x,
+            MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
 
     }
 }
