ad1a2bb1d28564d36b9f4a068535fdac3650e558
Felix <0911838@hr.nl>
2016-03-26 14:55:19 +0100

    Added config screen

--- Final version/Final_Application/Final_Application/Init.cs
+++ Final version/Final_Application/Final_Application/Init.cs
@@ -8,13 +8,81 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Final_Application
+namespace ATM
 {
-    public partial class Init : Form
+    public partial class Config : Form
     {
-        public Init()
+        public Config()
         {
             InitializeComponent();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            fetchCom();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Arduino.makePort(comboBox1.SelectedItem.ToString()) == true)
+                {
+                    StatusText.ForeColor = Color.Green;
+                    StatusText.Text = "ARDUINO CONNECTED";
+                    StartButton.Visible = true;
+                }
+                else
+                {
+                    StatusText.ForeColor = Color.Red;
+                    StatusText.Text = "NO CONNECTION";
+                }
+            }
+            catch (Exception)
+            {
+                Error.show("No port selected", "Error");
+            }
+        }
+
+        private void COMPORTCHOOSE_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void fetchCom()
+        {
+            String[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (String port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            if (ports.Length < 1)
+            {
+                comboBox1.Items.Add("No ports available");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KILLBUTTON_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Start frm = new Start();
+            frm.Show();
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            fetchCom();
         }
     }
 }
