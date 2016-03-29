d11cf5fbf220bcdaf5ed10040e7b93ef1d998a1b
Felix <0911838@hr.nl>
2016-03-29 15:40:15 +0200

    Added cancel option

--- Arduino_Final_Version/Arduino_Final_Version.ino
+++ Arduino_Final_Version/Arduino_Final_Version.ino
@@ -13,7 +13,7 @@ char keys[ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'*','0','#','$'}
 };
 byte rowPins[ROWS] = {7, 6, 5, 4}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {14, 15, 16, 17}; //connect to the column pinouts of the keypad
