05c4cecf34823e7674b4b0640242a09824240832
Felix <0911838@hr.nl>
2016-04-05 18:03:01 +0200

    Added corect final version for arduino

--- Arduino_Final_Version/Arduino_Final_Version.ino
+++ Arduino_Final_Version/Arduino_Final_Version.ino
@@ -47,17 +47,29 @@ void loop() {
     {
       if (mfrc522.PICC_ReadCardSerial())
       {
-        
-         dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-         byte readbackblock[18];
-         readBlock(18,readbackblock);
-         Serial.println("STARTING TO DUMP");
-         for(int i =0;i<15;i++)
-         {
-          Serial.println(readbackblock[i]);
-         }
-         Serial.println(",NEWUID");
-         delay(1000);
+        byte PasID[18];
+        byte RekeningID[18];
+        byte KlantID[18];
+        readBlock(62,PasID);
+        readBlock(61,RekeningID);
+        readBlock(60,KlantID);
+        for(int i=0;i<6;i++)
+        {
+          Serial.write(PasID[i]);
+        }
+        Serial.println();
+        for(int i=0;i<6;i++)
+        {
+          Serial.write(RekeningID[i]);
+        }
+        Serial.println();
+        for(int i=0;i<4;i++)
+        {
+          Serial.write(KlantID[i]);
+        }
+        Serial.println();
+      Serial.println(",NEWUID");
+      delay(1000);
       }
     }
     char key = keypad.getKey();
@@ -65,38 +77,4 @@ void loop() {
     Serial.print(key);
     Serial.println("KEY");
   }
-}
-
-int readBlock(int blockNumber, byte arrayAddress[]) 
-{
-  int largestModulo4Number=blockNumber/4*4;
-  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
-
-  /*****************************************authentication of the desired block for access***********************************************************/
-  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
-  //byte PCD_Authenticate(byte command, byte blockAddr, MIFARE_Key *key, Uid *uid);
-  //this method is used to authenticate a certain block for writing or reading
-  //command: See enumerations above -> PICC_CMD_MF_AUTH_KEY_A  = 0x60 (=1100000),    // this command performs authentication with Key A
-  //blockAddr is the number of the block from 0 to 15.
-  //MIFARE_Key *key is a pointer to the MIFARE_Key struct defined above, this struct needs to be defined for each block. New cards have all A/B= FF FF FF FF FF FF
-  //Uid *uid is a pointer to the UID struct that contains the user ID of the card.
-  if (status != MFRC522::STATUS_OK) {
-         Serial.print("PCD_Authenticate() failed (read): ");
-         Serial.println(mfrc522.GetStatusCodeName(status));
-         return 3;//return "3" as error message
-  }
-  //it appears the authentication needs to be made before every block read/write within a specific sector.
-  //If a different sector is being authenticated access to the previous one is lost.
-
-
-  /*****************************************reading a block***********************************************************/
-        
-  byte buffersize = 18;//we need to define a variable with the read buffer size, since the MIFARE_Read method below needs a pointer to the variable that contains the size... 
-  status = mfrc522.MIFARE_Read(blockNumber, arrayAddress, &buffersize);//&buffersize is a pointer to the buffersize variable; MIFARE_Read requires a pointer instead of just a number
-  if (status != MFRC522::STATUS_OK) {
-          Serial.print("MIFARE_read() failed: ");
-          Serial.println(mfrc522.GetStatusCodeName(status));
-          return 4;//return "4" as error message
-  }
-  Serial.println("block was read");
 }
--- Arduino_Final_Version/RFID_clean_test/clean_rfid/clean_rfid.ino
+++ Arduino_Final_Version/RFID_clean_test/clean_rfid/clean_rfid.ino
@@ -3,22 +3,45 @@
 #define RST_PIN 9
 #define SS_PIN 10
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
 void setup() 
 {
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
+  
+  for (byte i = 0; i < 6; i++) 
+  {
+    key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+  }
 }
 
 
-void loop() {
+void loop() 
+{
    if (mfrc522.PICC_IsNewCardPresent())
     {
       if (mfrc522.PICC_ReadCardSerial())
       {
         Serial.println("START READING BLOCKS");
-        MFRC522::MIFARE_Key key;
-        for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+        byte PasID[18];
+        byte RekeningID[18];
+        byte KlantID[18];
+        readBlock(62,PasID);
+        readBlock(61,RekeningID);
+        readBlock(60,KlantID);
+        for(int i=0;i<6;i++)
+        {
+          Serial.write(PasID[i]);
+        }
+        for(int i=0;i<6;i++)
+        {
+          Serial.write(RekeningID[i]);
+        }
+        for(int i=0;i<4;i++)
+        {
+          Serial.write(KlantID[i]);
+        }
       }
-}
+    }
 }
