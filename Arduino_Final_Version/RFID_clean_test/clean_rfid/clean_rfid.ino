21be5642febbc8a6710f10628324efdc9a5e8b3e
CHG1731 <felixos_11@hotmail.com>
2016-04-06 02:46:50 +0200

    Changed arduino program so that C# recognizes the values

--- Arduino_Final_Version/RFID_clean_test/clean_rfid/clean_rfid.ino
+++ Arduino_Final_Version/RFID_clean_test/clean_rfid/clean_rfid.ino
@@ -1,9 +1,24 @@
 #include <MFRC522.h>
 #include <SPI.h>
+#include <Keypad.h>
 #define RST_PIN 9
 #define SS_PIN 10
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
+byte nuidPICC[3];
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','$'}
+};
+byte rowPins[ROWS] = {7, 6, 5, 4}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {14, 15, 16, 17}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
 void setup() 
 {
   Serial.begin(9600);
@@ -23,7 +38,6 @@ void loop()
     {
       if (mfrc522.PICC_ReadCardSerial())
       {
-        Serial.println("START READING BLOCKS");
         byte PasID[18];
         byte RekeningID[18];
         byte KlantID[18];
@@ -34,14 +48,24 @@ void loop()
         {
           Serial.write(PasID[i]);
         }
+        Serial.print("\n");
         for(int i=0;i<6;i++)
         {
           Serial.write(RekeningID[i]);
         }
+        Serial.print("\n");
         for(int i=0;i<4;i++)
         {
           Serial.write(KlantID[i]);
         }
+        Serial.print("\n");
+        Serial.println(",NEWUID");
+        delay(2000);
       }
     }
+        char key = keypad.getKey();
+      if (key != NO_KEY){
+    Serial.print(key);
+    Serial.println("KEY");
+  }
 }
